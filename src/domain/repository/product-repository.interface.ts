5b6c9c3ac231f291a86267f95b398471c1528471
Rafael Oliveira <rafael8paulo@gmail.com>
2024-07-09 14:44:17 -0300

    Estendendo camada de infra

---
+++ src/domain/repository/product-repository.interface.ts
@@ -0,0 +1,8 @@
+import Product from "../entity/product";
+import RepositoryInterface from "./repository-interface";
+
+export default interface ProductRespositoryInterface extends RepositoryInterface<Product> {
+    
+
+
+}
