25258eda2b11fa87368483df5b7ba5c6d3698282
Rafael Oliveira <rafael8paulo@gmail.com>
2024-07-09 14:09:35 -0300

    Adicionando programa de rewards

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,6 +1,7 @@
 import OrderItem from "../entity/order_item";
 import Order from "../entity/order";
 import OrderService from "./order.service";
+import Customer from "../entity/customer";
 
 describe("Order service unit tests", () => {
 
@@ -16,5 +17,17 @@ describe("Order service unit tests", () => {
         expect(total).toBe(500);
 
     });
+
+    it("should place an order", () => {
+        const customer = new Customer("c1", "Customer 1");
+        const item1 = new OrderItem("i1", "Item 1", 10, 1, "p1");
+
+        const order = OrderService.placeOrder(customer, [item1]);
+
+        expect(customer.rewardPoints).toBe(5);
+        expect(order.total()).toBe(10);
+
+    })
+
 
 });
